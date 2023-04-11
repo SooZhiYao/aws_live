9db9248e89d68dcf351e8d06d303c3eb4df76e63
SooZhiYao <100904467+SooZhiYao@users.noreply.github.com>
2023-04-11 11:11:57 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-2.cupqwfs7wt88.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "soozhiyao_employee"
+custombucket = "soozhiyao_employee"
+customregion = "us-east-1"
